50de46f374fd2e3f974b12e578200dcbadce5737
Vinny Burakovas <vini.fcatlive.com>
2020-05-07 13:04:13 +0200

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Blog from '../views/Blog.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
